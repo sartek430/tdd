347066ec127fe4d7cafba926bec30ae876cd1627
sartek430 <macogneur430@gmail.comm>
2025-01-17 12:00:26 +0100

    delete app.ts and dotenv

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import userRoutes from "./routes/userRoutes";
-
-const app = express();
-app.use(express.json());
-app.use("/users", userRoutes);
-
-export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+import userRoutes from "./routes/userRoutes";
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.use(express.json());
+app.use("/users", userRoutes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
